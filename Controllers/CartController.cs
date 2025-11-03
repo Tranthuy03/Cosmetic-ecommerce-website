dc2b64bfd210f595416943e218f36a67d05a1dfc
duynguyeneiu <155942227+duynguyeneiu@users.noreply.github.com>
2025-11-03 10:19:55 +0700

    cart
    v1

---
+++ Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using HairCareStore.Data;
+using HairCareStore.Models;
+using HairCareStore.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairCareStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly EshopContext _context;
+        public CartController(EshopContext context)
+        {
+            _context = context;
+        }
+        const string CART_KEY = "MYCART";
+
+        public List<CartItem> Cart => HttpContext.Session.Get<List<CartItem>>(CART_KEY) ?? new List<CartItem>();
+
+        public IActionResult Index()
+        {
+            return View(Cart);
+        }
+        public IActionResult AddToCart(int id, int quantity=1)
+        {
+            var tempCart = Cart;
+            var item = tempCart.SingleOrDefault(p => p.ProductId == id);
+            if (item == null)
+            {
+                var product = _context.Products.SingleOrDefault(p => p.ProductId == id);
+                if (product == null)
+                {
+                    TempData["Message"] = $"Not found productID: {id}";
+                    return Redirect("/404");
+                }
+                item = new CartItem
+                {
+                    ProductId = product.ProductId,
+                    Name = product.Name,
+                    Price = (double)product.Price,
+                    ImageUrl = product.ImageUrl,
+                    Quantity = quantity,
+                };
+                tempCart.Add(item);
+
+            }
+            else {
+                item.Quantity += quantity;
+            }
+            HttpContext.Session.Set(CART_KEY, tempCart);
+
+                return RedirectToAction("Index");
+        }
+    }
+}
