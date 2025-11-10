5286568c4f53c958fa55311bbf098ea10167dc6d
duynguyeneiu <155942227+duynguyeneiu@users.noreply.github.com>
2025-11-10 11:05:03 +0700

    fix cart

---
+++ ViewComponents/CartViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using HairCareStore.Helper;
+using HairCareStore.Models;
+using HairCareStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairCareStore.ViewComponents
+{
+    public class CartViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            var cart=HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
+
+            return View("CartPanel",new CartModel
+            {
+                Quantity=cart.Sum(x => x.Quantity),
+                Total=cart.Sum(x => x.SubTotal)
+
+            });
+        }
+    }
+}
