ea724f9d93a2257f4aee7623f1365f8d1c8a7eaf
duynguyeneiu <155942227+duynguyeneiu@users.noreply.github.com>
2025-12-04 10:33:21 +0700

    Fix cart and add COD

---
+++ wwwroot/SQLQuery_add bill.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE Invoice (
+    InvoiceId INT NOT NULL PRIMARY KEY,
+    UserId NVARCHAR(20) NULL,
+    OrderDate DATETIME NULL,
+    ExpectedDeliveryDate DATETIME NULL,
+    ActualDeliveryDate DATETIME NULL,
+    Name NVARCHAR(50) NULL,
+    Address NVARCHAR(60) NULL,
+    Payment NVARCHAR(50) NULL,
+    ShippingCarrier NVARCHAR(50) NULL,
+    ShippingCost FLOAT NULL,
+    StatusId INT NULL,
+    GhiChu NVARCHAR(50) NULL
+);
+GO
+
+CREATE TABLE Status (
+    StatusId INT NOT NULL PRIMARY KEY,
+    Name NVARCHAR(50) NULL,
+    Description NVARCHAR(500) NULL
+);
+GO
+INSERT INTO Status (StatusId, Name, Description) VALUES 
+(-1, N'Khách hàng hủy đơn hàng', NULL),
+(0, N'Mới đặt hàng', NULL),
+(1, N'Đã thanh toán', NULL),
+(2, N'Chờ giao hàng', NULL),
+(3, N'Đã giao hàng', NULL);
+GO
+
+
+DELETE FROM Status;
+GO
