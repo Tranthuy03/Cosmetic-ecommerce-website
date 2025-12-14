cbb348243dd8af53a1ec0798235466eccc98279f
duynguyeneiu <155942227+duynguyeneiu@users.noreply.github.com>
2025-12-14 19:56:41 +0700

    ss

--- obj/Debug/net8.0/HairCareStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/HairCareStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HairCareStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2086962ef03bd97dd2d38d1535b5c6ddbae24427")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a23d5b1cd8f76559440086afd8cd4f8e763de6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HairCareStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HairCareStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
